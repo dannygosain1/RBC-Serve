1e5e7bee895df9103649f85578751730e1d92aa5
Sanhar Balachandran <s6balach@uwaterloo.ca>
2016-09-24 22:53:40 -0400

    server

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+$(function() {
 
+     $.getJSON("/Analytics", function(data){
+        chart1 = data['chart']
+        series1 = data['series']
+        title1 = data['title']
+        xAxis1 = data['xAxis']
+        yAxis1 = data['yAxis']
+        $("#container").highcharts({
+            chart: chart1,
+            title: title1,
+            xAxis: xAxis1,
+            yAxis: yAxis1,
+            series: series1
+        });
+    })
 });
