bcd79d459939bfd5714fab1f5aecf562029642da
bainian12345 <bainian12345@hotmail.com>
2016-09-25 01:41:00 -0400

    added job.js

---
+++ static/js/jobs.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	submitsearch.click(function() {
+		let service = $("#servicesearch").val(),
+				location = $("#locationsearch").val(),
+				reqstring = '/api/search_posts?location="'+location+'"&service="'+service+'"';
+		$.post(reqstring, function(data, status){
+			if (status == "success") {
+				let joblist = $(".job-list ul")
+				$(".submit-status").innerHTML = "Submitted successfully.";
+					data.forEach(function(job) {
+						let industry = document.createElement("DIV"),
+								serviceText = job.service,
+								description = document.createElement("DIV"),
+								descriptionText = job.description,
+								budget = document.createElement("DIV"),
+								budgetText = job.budget,
+								listitem = document.createElement("LI");
+						industry.appendChild(serviceText);
+						description.appendChild(descriptionText);
+						budget.appendChild(budgetText);
+						listitem.appendChild(industry).appendChild(description).appendChild(budget);
+						joblist.appendChild(listitem);
+				});
+			} else {
+				$(".submit-status").innerHTML = "Submission failed. Please try again";			
+			}
+		});		
+	});	
+});
