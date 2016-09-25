7eda72e3afd81923a38afcffe4f75b87e919f62f
Sadruddin Hashmani <sadu99@hotmail.com>
2016-09-25 08:59:09 -0400

    updated jobs.js

--- static/js/jobs.js
+++ static/js/jobs.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+	$.get('/api/get_posts?', function(data, status) {
+		var jsonObj = $.parseJSON('['+data+']');
+		if (status == "success") {
+			document.getElementById("job-list").innerHTML = "";
+			jsonObj.forEach(function(job) {
+				document.getElementById("job-list").innerHTML += '<li>'+
+					'<div class="industry">'+jsonObj[0].service+'</div>'+
+					'<div class="dscription">'+jsonObj[0].description+'</div'+
+					'<div class="budget">'+jsonObj[0].budget+'</div>'+
+					'</li>';
+			});
+		} 
+	});
+
 	var submitsearch = $("#submitsearch");
 	submitsearch.click(function() {
 		let service = $("#servicesearch").val(),
