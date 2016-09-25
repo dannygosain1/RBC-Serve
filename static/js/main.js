9eee5ba3a948ddc857d6636f60068a21e94b2473
bainian12345 <bainian12345@hotmail.com>
2016-09-25 02:41:22 -0400

    Clean up new "jobs" page

--- static/js/main.js
+++ static/js/main.js
@@ -1,38 +1,4 @@
 $(document).ready(function() {
-
-	var clienttab = $("#client"),
-		providertab = $("#provider"),
-	    clientpage = $("#client-page"),
-	    providerpage = $("#provider-page"),
-	    diffaddress = $("#diffaddress"),
-	    submitpost = $("#submitpost"),
-	    submitsearch = $("#submitsearch");
-
-	clienttab.click(function() {
-		if(clientpage.hasClass("hidden-content")) {
-			clienttab.addClass("active");
-			providertab.removeClass("active");
-			clientpage.removeClass("hidden-content");
-			providerpage.addClass("hidden-content");			
-		}
-	}); 
-	providertab.click(function() {
-		if(providerpage.hasClass("hidden-content")) {
-			providertab.addClass("active");
-			clienttab.removeClass("active");
-			providerpage.removeClass("hidden-content");
-			clientpage.addClass("hidden-content");			
-		}		
-	}); 
-	diffaddress.click(function() {
-		let addressfields = $("#addressfields")
-		if(addressfields.hasClass("hidden-content")) {
-			addressfields.removeClass("hidden-content");
-		}	else {
-			addressfields.addClass("hidden-content");			
-		}
-	});
-
 	submitpost.click(function() {
 		let	formdata = {
 					service : $("#servicelist").val(),
